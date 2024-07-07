c75e05c471df83a74beda1214eeab5cc6b286aab
Rizquez <pedro.rizquez.94@gmail.com>
2024-07-07 17:48:07 +0200

    Creacion base del backend

---
+++ jummy-back/main.py
@@ -0,0 +1,7 @@
+from src.routes import app
+from config import current_config
+
+app.config.from_object(current_config)
+
+if __name__ == '__main__':
+    app.run(host=current_config.HOST, port=current_config.PORT)
---
+++ jummy-back/src/routes/routes.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, redirect, request, send_file, jsonify
+from ..tools import MySQL
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+
+    a = MySQL.create_engine_mysql()
+    b = MySQL.simple_query(a, 'select cif from restaurantes where id=:id', params= {'id': 41})
+    print(b)
+
+    return '<h1>hola</h1>'
